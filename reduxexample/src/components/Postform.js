b6f61cd351890d50b966d28657717059a9827cc4
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-23 13:00:28 +0100

    post + postform components

---
+++ reduxexample/src/components/Postform.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import Posts from './Posts'
+
+class Postform extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Add Post</h1>
+      </div>
+    )
+  }
+}
+
+export default Postform
