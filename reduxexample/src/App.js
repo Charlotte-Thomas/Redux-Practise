3d0a3ce7a8fbd82d1ad9927b2be9935a5f11b037
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-23 13:54:15 +0100

    redux added

--- reduxexample/src/App.js
+++ reduxexample/src/App.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
 import Posts from './components/Posts'
 import Postform from './components/Postform'
 
+const store = createStore(() => [], {}, applyMiddleware())
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
         </a>
-      </header>
-      <Postform />
-      <hr />
-      <Posts />
-    </div> 
+        </header>
+        <Postform />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
