0615d0cfcb9b6f666b15349626216447c2201d22
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-23 13:12:01 +0100

    forms added

--- reduxexample/src/components/Postform.js
+++ reduxexample/src/components/Postform.js
@@ -6,13 +6,37 @@ class Postform extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      title: '',
+      body: ''
     }
+    this.onChange = this.onChange.bind(this)
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
   }
 
   render() {
     return (
       <div>
         <h1>Add Post</h1>
+        <form>
+          <div>
+            <label>Title: </label><br />
+            <input type='text' name='title'
+              onChange={this.onChange}
+              value={this.state.title} />
+          </div>
+          <br />
+          <div>
+            <label>Body: </label><br />
+            <textarea name='body'
+              value={this.state.body}
+              onChange={this.onChange} />
+          </div>
+          <br />
+          <button type='submit'>submit</button>
+        </form>
       </div>
     )
   }
