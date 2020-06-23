3bd25242079348323ee08567924516c18f2873a1
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-23 15:37:14 +0100

    Postform uses redux state

--- reduxexample/src/components/Postform.js
+++ reduxexample/src/components/Postform.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
-import Posts from './Posts'
-import axios from 'axios'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { createPost } from '../actions/postActions'
 
 class Postform extends Component {
 
@@ -24,15 +25,7 @@ class Postform extends Component {
       title: this.state.title,
       body: this.state.body
     }
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      headers: {
-        'content-type': 'application/json'
-      },
-      body: JSON.stringify(post)
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
+    this.props.createPost(post)
 
   }
 
@@ -62,4 +55,8 @@ class Postform extends Component {
   }
 }
 
-export default Postform
+Postform.propTypes = {
+  createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, { createPost })(Postform)
