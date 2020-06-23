a973f0348ac29de9a19ffad4e2ce458ec0bfff62
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-06-23 14:35:40 +0100

    Posts uses redux state

--- reduxexample/src/actions/postActions.js
+++ reduxexample/src/actions/postActions.js
@@ -1,9 +1,9 @@
 import { FETCH_POSTS, NEW_POST } from './types'
 
 export const fetchPosts = () => dispatch => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+  fetch('https://jsonplaceholder.typicode.com/posts')
     .then(res => res.json())
-    .then(post => dispatch({
+    .then(posts => dispatch({
       type: FETCH_POSTS,
       payload: posts
     }))
